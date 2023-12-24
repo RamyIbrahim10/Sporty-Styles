2a842bb0f13978334aedfc896dff20d750fa3bfd
Rami Ibrahim <ramy.ayman68@gmail.com>
2023-12-25 02:02:02 +0300

    connected with database successfully

--- phpPages/SignUpAsUser.php
+++ phpPages/SignUpAsUser.php
@@ -1,5 +1,4 @@
 <?php
-
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -16,7 +15,8 @@ if ($conn->connect_error) {
 }
 
 // Function to safely escape input values
-function clean_input($value) {
+function clean_input($value)
+{
     global $conn;
     return mysqli_real_escape_string($conn, stripslashes($value));
 }
@@ -24,11 +24,14 @@ function clean_input($value) {
 // Handle form submission
 if (isset($_POST["submit"])) {
     // Fetch user input
-    $username = clean_input($_POST["email"]);
-    $password = clean_input($_POST["password"]);
+    $username = clean_input($_POST["fullName"]);
+    $email = clean_input($_POST["email1"]);
+    $password = clean_input($_POST["pass"]);
+    $dob = clean_input($_POST["dob"]);
+    $gender = clean_input($_POST["gender"]);
 
     // Insert user details into the database
-    $sql = "INSERT INTO signupasuser (email, password) VALUES ('$username', '$password')";
+    $sql = "INSERT INTO signupasuser (fullName, email, Password, DateofBirth, Gender) VALUES ('$username', '$email', '$password', '$dob', '$gender')";
     if ($conn->query($sql) === true) {
         echo "Sign-up successful!";
     } else {
