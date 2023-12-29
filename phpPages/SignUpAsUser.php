38fe2bbfc6654559a0418fcfa1f583ec6848250e
Rami Ibrahim <ramy.ayman68@gmail.com>
2023-12-29 16:00:40 +0300

    .

--- phpPages/SignUpAsUser.php
+++ phpPages/SignUpAsUser.php
@@ -27,7 +27,7 @@ if (isset($_POST["submit"])) {
     // Insert user details into the database
     $sql = "INSERT INTO signupasuser (fullName, email, Password, DateofBirth, Gender) VALUES ('$username', '$email', '$password', '$dob', '$gender')";
     if ($conn->query($sql) === true) {
-        echo "Sign-up successful!";
+        header("Location: ../ShoppingCenterManagement/fork.html");
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
