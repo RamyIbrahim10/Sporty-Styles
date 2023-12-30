87045bad9e38fead60197824d962061d92afd59d
Rami Ibrahim <ramy.ayman68@gmail.com>
2023-12-30 12:48:17 +0300

    .

--- phpPages/LogInAsUser.php
+++ phpPages/LogInAsUser.php
@@ -16,19 +16,19 @@ if ($conn->connect_error) {
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
-    $password = $_POST['password'];
+    $password = $_POST['Password'];
 
     // SQL query to retrieve user from the database
-    $sql = "SELECT * FROM signupasuser WHERE username = '$username'";
+    $sql = "SELECT * FROM signupasuser WHERE email = '$username'";
     $result = $conn->query($sql);
 
     if ($result->num_rows == 1) {
         // User found
         $row = $result->fetch_assoc();
         // Verify the entered password with the hashed password in the database
-        if (password_verify($password, $row['password'])) {
+        if (password_verify($password, $row['Password'])) {
             // Password is correct, login successful
-            $_SESSION['username'] = $username; // Store username in session for further use
+            $_SESSION['email'] = $username; // Store username in session for further use
             echo "Login successful!";
             // Redirect to a new page or set session variables for logged-in user
         } else {
