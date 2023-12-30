707940d73afeee87280d4edc528801ef595b3280
Rami Ibrahim <ramy.ayman68@gmail.com>
2023-12-30 14:44:28 +0300

    .

--- phpPages/LogInAsProvider.php
+++ phpPages/LogInAsProvider.php
@@ -1,17 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
-
-
-    <!-- Bootstrap JavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
-
 <?php
 session_start();
 
@@ -32,6 +18,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['Password'];
 
+    // Check if the user is blocked
+    if (isUserBlocked($username)) {
+        echo "Sorry, your account is blocked. Please contact support for assistance.";
+        exit();
+    }
+
     // SQL query to retrieve user from the database
     $sql = "SELECT * FROM signupasprovider WHERE email = '$username'";
     $result = $conn->query($sql);
@@ -44,18 +36,49 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $_SESSION['email'] = $username;
             echo "<script>alert('Login successful!');</script>";
             echo "<script>window.location.href = '../htmlPages/fork.html';</script>"; // Redirect to a dashboard page
+            resetLoginAttempts($username); // Reset login attempts on successful login
             exit();
         } else {
             // Invalid password
-            echo "<script>alert('Invalid Password');</script>";
-            echo "<script>window.location.href = '../htmlPages/LoginUser.html';</script>";
-            exit();
+            incrementLoginAttempts($username); // Increment login attempts for this user
+            if (loginAttemptsExceeded($username)) {
+                blockUser($username); // Block the user if login attempts exceed the threshold
+                echo "<script>alert('Sorry, your account has been blocked due to multiple failed login attempts. Please contact support for assistance.');</script>";
+                exit();
+            } else {
+                echo "<script>alert('Invalid Password');</script>";
+                echo "<script>window.location.href = '../htmlPages/LoginUser.html';</script>";
+                exit();
+            }
         }
     } else {
         // User not found
-
+        // Handle user not found scenario if needed
     }
 }
 
 $conn->close();
+
+// Functions for login attempt limit
+function isUserBlocked($username) {
+    // Implement logic to check if the user is blocked in the database
+    // Return true if the user is blocked, false otherwise
+}
+
+function resetLoginAttempts($username) {
+    // Implement logic to reset login attempts for the user in the database
+}
+
+function incrementLoginAttempts($username) {
+    // Implement logic to increment login attempts for the user in the database
+}
+
+function loginAttemptsExceeded($username) {
+    // Implement logic to check if login attempts exceed the threshold
+    // Return true if exceeded, false otherwise
+}
+
+function blockUser($username) {
+    // Implement logic to block the user in the database
+}
 ?>
