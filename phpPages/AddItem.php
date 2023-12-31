301be6b411df17d2b7f132868d9cf7af60532a06
Rami Ibrahim <ramy.ayman68@gmail.com>
2023-12-31 04:28:13 +0300

    ..

--- phpPages/AddItem.php
+++ phpPages/AddItem.php
@@ -35,69 +35,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $manufacturingLocation = $_POST['MadeIn'];
     }
 
-    // Check if file input 'itemImage' exists in the form submission
-    if(isset($_FILES['image']) && $_FILES['image']['error'] === UPLOAD_ERR_OK) {
-        $targetDir = "uploads/";
+    $targetDir = "uploads/"; // Directory where images will be stored
+    $targetFile = $targetDir . basename($_FILES["image"]["name"]);
 
-        // Create the directory if it doesn't exist
-        if (!file_exists($targetDir)) {
-            mkdir($targetDir, 0777, true);
-        }
+    // Check if file already exists
+    if (file_exists($targetFile)) {
+        echo "Sorry, file already exists.";
+    } else {
+        // Try to upload the file
+        if (move_uploaded_file($_FILES["image"]["tmp_name"], $targetFile)) {
+            // File uploaded successfully, save the file name or path to the database
+            $imagePath = $targetFile;
 
-        $targetFile = $targetDir . basename($_FILES["image"]["name"]);
-        $absoluteTargetFile = __DIR__ . '/' . $targetFile; // Use absolute path
-
-        if (move_uploaded_file($_FILES["image"]["tmp_name"], $absoluteTargetFile)) {
-            $itemImage = $targetFile;
-
-            // Prepare the SQL statement
-            $stmt = $conn->prepare("INSERT INTO items (image, Name, Price, AfterDiscount, PDate, MadeIn) VALUES (?, ?, ?, ?, ?, ?)");
-
-            if ($stmt) {
-                // Bind parameters to the prepared statement
-                $stmt->bind_param("ssssss", $itemImage, $itemName, $itemPrice, $productionDate, $productionDate, $manufacturingLocation);
-
-                // Execute the statement
-                if ($stmt->execute()) {
-                    echo "Data stored successfully!";
-                } else {
-                    echo "Error executing the statement: " . $stmt->error;
-                }
-
-                $stmt->close();
+            // Insert image path into the database
+            $sql = "INSERT INTO images (image_path) VALUES ('$imagePath')";
+            if ($conn->query($sql) === TRUE) {
+                echo "Image uploaded and saved in the database successfully.";
             } else {
-                echo "Error preparing the statement: " . $conn->error;
+                echo "Error: " . $sql . "<br>" . $conn->error;
             }
         } else {
-            echo "Error uploading file.";
-        }
-    } else {
-        // Check specific errors in $_FILES['image']['error']
-        switch ($_FILES['image']['error']) {
-            case UPLOAD_ERR_INI_SIZE:
-                echo "The uploaded file exceeds the upload_max_filesize directive in php.ini";
-                break;
-            case UPLOAD_ERR_FORM_SIZE:
-                echo "The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form";
-                break;
-            case UPLOAD_ERR_PARTIAL:
-                echo "The uploaded file was only partially uploaded";
-                break;
-            case UPLOAD_ERR_NO_FILE:
-                echo "No file was uploaded";
-                break;
-            case UPLOAD_ERR_NO_TMP_DIR:
-                echo "Missing a temporary folder";
-                break;
-            case UPLOAD_ERR_CANT_WRITE:
-                echo "Failed to write file to disk";
-                break;
-            case UPLOAD_ERR_EXTENSION:
-                echo "A PHP extension stopped the file upload";
-                break;
-            default:
-                echo "Unknown error occurred with the file upload";
-                break;
+            echo "Sorry, there was an error uploading your file.";
         }
     }
 
